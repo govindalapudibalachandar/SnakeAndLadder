db7c59e42a60ab566329db545aca0d270743dbe3
balachandar <govindalapudibalachandar@gmail.com>
2021-08-19 12:31:32 +0530

    Added:Snakeandladder file is added

--- src/com/bridgelaz/SnakeAndLadder.java
+++ src/com/bridgelaz/SnakeAndLadder.java
@@ -1,10 +1,13 @@
 package com.bridgelaz;
 
 public class SnakeAndLadder {
+	
+	  public static final int position =0 ;
 
 	public static void main(String[] args) {
 		
 		System.out.println("Welcome to SnakeAndLadder");
+		System.out.println("playerposition : " +position);
 
 	}
 
